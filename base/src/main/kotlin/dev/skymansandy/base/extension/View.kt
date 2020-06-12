d1bf5059cbbba28ec83fa07e992c198a2238aed2
skymansandy <sandeshbaliga96@gmail.com>
2020-06-12 22:09:51 +0530

    Press back to exit implemented

--- base/src/main/kotlin/dev/skymansandy/base/extension/View.kt
+++ base/src/main/kotlin/dev/skymansandy/base/extension/View.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
 
 fun View.cancelTransition() {
     transitionName = null
